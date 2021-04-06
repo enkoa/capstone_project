eab22ab8d3706b69cd152622f3d0eefb7e400af8
Bao Le <59161665+enkoa@users.noreply.github.com>
2021-04-05 20:46:27 -0500

    add main and variable terraform file

---
+++ main.tf
@@ -0,0 +1,77 @@
+provider "google" {
+	credentials	= file("credentials.json")
+	project		= var.project_id
+	region		= var.region
+}
+
+module "gke" {
+	source 			= "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+
+	project_id			= var.project_id
+	name				= var.cluster_name
+	region			= var.region
+	network			= var.network
+	subnetwork			= var.subnetwork
+	ip_range_pods		= var.ip_range_pods
+	ip_range_services	= var.ip_range_services
+
+	create_service_account = false
+	remove_default_node_pool = true
+
+	node_pools = [
+		{
+			name 			= "default-node-pool"
+			machine_type 	= var.machine_type
+			min_count		= var.min_count
+			max_count		= var.max_count
+			disk_size_gb 	= var.disk_size_gb
+			disk_type		= var.disk_type
+			image_type		= var.image_type
+			auto_repair	= true
+			auto_upgrade	= true
+			service_account = var.service_account
+			preemptible	= true
+			initial_node_count = var.initial_node_count
+		},
+	]
+	node_pools_oauth_scopes = {
+    		all = []
+   		default-node-pool = [
+      		"https://www.googleapis.com/auth/cloud-platform",
+    		]
+ 	}
+
+  	node_pools_labels = {
+    		all = {}
+		default-node-pool = {
+      		default-node-pool = true
+    		}
+  	}
+
+  	node_pools_metadata = {
+    		all = {}
+    		default-node-pool = {
+      		node-pool-metadata-custom-value = "my-node-pool"
+    		}
+  	}
+
+  	node_pools_taints = {
+    		all = []
+    		default-node-pool = [
+      	{
+        		key    = "default-node-pool"
+        		value  = true
+        		effect = "PREFER_NO_SCHEDULE"
+      	},
+    		]
+  	}
+
+  	node_pools_tags = {
+    		all = []
+    		default-node-pool = [
+      		"default-node-pool",
+    		]
+  	}
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,60 @@
+# Provider info
+# Enter your project ID into the REPLACE_ME section
+variable "project_id" {
+	default		= "REPLACE_ME"
+}
+variable "region" {
+	default		= "us-central1"
+}
+
+# Cluster specs
+# Feel fre to change any of the fields to meet your respective 
+# cluster specs.
+#
+# Enter your Service Accoun into the REPLACE_ME section
+variable "cluster_name" {
+	default		= "my-gke-cluster"
+}
+variable "machine_type" {
+	default		= "n1-standard-1"
+}
+variable "min_count" {
+	default		= 1
+}
+variable "max_count" {
+	default		= 3
+}
+variable "disk_size_gb" {
+	default		= 50
+}
+variable "disk_type" {
+	default		= "pd-standard"
+}
+variable "image_type" {
+	default		= "COS"
+}
+variable "initial_node_count" {
+	default		= 1
+}
+variable "service_account" {
+	default		= "REPLACE_ME"
+}
+
+variable "ip_range_pods" { 
+	default 		= ""
+}
+variable "ip_range_services" {
+	default		= ""
+}
+variable "network" { 
+	default 		= "default"
+}
+variable "subnetwork" {
+	default		= ""
+}
+
+
+
+
+
+
